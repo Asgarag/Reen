fcac07faec33255bc843fb709ed2fe5f060e643a
Andrei <kozinadnrew1@gmail.com>
2018-01-06 19:28:06 +0300

    Сделал 2 следующих блока

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,6 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     sourcemaps = require('gulp-sourcemaps'),
     rigger = require('gulp-rigger'),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
     browserSync = require("browser-sync"),
 	cssnano = require('cssnano'),
 	htmlmin = require('gulp-htmlmin'),
@@ -13,6 +11,7 @@ var gulp = require('gulp'),
 	cssnext = require('postcss-cssnext'),
 	isimport = require('postcss-import'),
 	rename = require('gulp-rename'),
+	rimraf = require('rimraf'),
     reload = browserSync.reload;
 
 var path = {
@@ -20,22 +19,19 @@ var path = {
         html: 'build/',
         js: 'build/js/',
         css: 'build/css/',
-        img: 'build/img/',
-        fonts: 'build/fonts/'
+        img: 'build/img/'
     },
     src: { 
         html: 'src/*.html',
         js: 'src/js/main.js',
         style: 'src/style/main.css',
-        img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+        img: 'src/img/mini/*.*'
     },
     watch: {
         html: 'src/**/*.html',
         js: 'src/js/**/*.js',
         style: 'src/style/**/*.css',
-        img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+        img: 'src/img/mini/*.*'
     },
     clean: './build'
 };
@@ -69,36 +65,24 @@ gulp.task('style:build', function () {
     gulp.src(path.src.style)
         .pipe(sourcemaps.init())
 		.pipe(rename('style.css'))
-        .pipe(postcss([isimport(), cssnext({features: {rem: false}})]))
+        .pipe(postcss([isimport(), cssnext({features: {rem: false}}), cssnano({autoprefixer: false})]))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(path.build.css))
         .pipe(reload({stream: true}));
-});
-
-gulp.task('image:build', function () {
-    gulp.src(path.src.img)
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()],
-            interlaced: true
-        }))
-        .pipe(gulp.dest(path.build.img))
-        .pipe(reload({stream: true}));
-});
-
-gulp.task('fonts:build', function() {
-    gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts))
 });
 
 gulp.task('build', [
     'html:build',
     'js:build',
     'style:build',
-    'fonts:build',
-    'image:build'
+	'image:build'
 ]);
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img) 
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({stream: true}));
+});
 
 gulp.task('watch', function(){
     watch([path.watch.html], function(event, cb) {
@@ -110,11 +94,8 @@ gulp.task('watch', function(){
     watch([path.watch.js], function(event, cb) {
         gulp.start('js:build');
     });
-    watch([path.watch.img], function(event, cb) {
+	watch([path.watch.img], function(event, cb) {
         gulp.start('image:build');
-    });
-    watch([path.watch.fonts], function(event, cb) {
-        gulp.start('fonts:build');
     });
 });
 
